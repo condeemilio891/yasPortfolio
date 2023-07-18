ecfbeef4aa8c076d4a4d87147d0cbba666893931
Emilio Conde <condeemilio891@gmail.com>
2023-07-18 16:13:28 -0500

    Updates

--- yasportfolio/src/Components/backgorundSlider/data.js
+++
@@ -1,9 +0,0 @@
-const imageSlide=[
-    {url:"../../assets/tatPic1.png", title:"tatPic1"},
-    {url:"../../assets/tatPic2.png", title:"tatPic1"},
-    {url:"../../assets/tatPic3.png", title:"tatPic1"},
-   
-]
-
-
-export default imageSlide
